bf7b2a91dc8d125a7a438ce5fcc0bb63973c5b01
Chordey213 <cpwalla026@gmail.com>
2018-03-15 19:34:32 -0400

    Created server, and added MLab DB

---
+++ MyStatus/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    appName: 'MyStatus',
+    port: 3000,
+    db: {
+      username: "MCP",
+      password: "TR0N",
+      host: 'ds125198.mlab.com:25198',
+      dbName: 'chordey213',
+    }
+  };
+  

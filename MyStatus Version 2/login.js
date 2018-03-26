a5a5031e2be7c745b8aa60fa497f0f18873b45cd
Chordey213 <cpwalla026@gmail.com>
2018-03-25 21:52:04 -0400

    added the mongo server and mongoose requirements to the main page

--- MyStatus Version 2/login.js
+++ MyStatus Version 2/login.js
@@ -1,8 +1,17 @@
 const express = require('express')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 const app = express()
+
+//mongodb connection
+mongoose.connect("mongodb://localhost:27017/users");
+var db = mongoose.connection
+//mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, function(){
 	console.log("Express server is listening on port", 3000);
 });
+
